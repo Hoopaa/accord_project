4b8f527a284a5f6580fad82cedcb06a493030361
gerome12 <gerome.pasquier@gmail.com>
2017-05-06 23:11:23 +0200

    fin du parsing de A à Z

--- parsing/getPartitionV2.py
+++ parsing/getPartitionV2.py
@@ -119,7 +119,7 @@ def getAccord(url):
 
 #alphabet = ["9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-alphabet = ["M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+alphabet = ["T","U","V","W","X","Y","Z"]
 
 #pour tous les artistes (de A a Z + 9)
 for lettre in alphabet:
