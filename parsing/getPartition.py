4685259ce805f99e5a40578497fd6a89c073ae48
gerome12 <gerome.pasquier@gmail.com>
2017-05-07 14:03:57 +0200

    .

--- parsing/getPartition.py
+++ parsing/getPartition.py
@@ -2,69 +2,50 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import time
-import logging
-import json
 from urllib2 import urlopen
 import bs4 as BeautifulSoup
-import unicodedata
-import sqlite3
+import time
+
 import re
-from pymongo import Connection
 
-class dbMongo:
+def request(url):
+    res = False
+    while res == False:
+        try:
+            j = urlopen(url).read()
+            res = True
+        except Exception as e:
+            print e.message
+            print e.args
 
-    def __init__(self):
-        self.connection = Connection('127.0.0.1', 27017)
-        self.db = self.connection.mydatabase2
-        self.db['song'].drop()
-        self.db['artiste'].drop()
+            #en cas de probleme attendre 20 secondes
+            time.sleep(20)
 
+    return BeautifulSoup.BeautifulSoup(j,"lxml")
 
-    def insertSongMongo(self, song, id):
-        Tsong={'id':id, }
-        self.db['song'].insert(Tsong)
+class output:
 
-    def insertArtisteMongo(self, artiste, id):
-        Tartiste={'id':id, 'name':artiste.name, 'link':artiste.link, 'nbSong':artiste.nbSong}
-        self.db['artiste'].insert(Tartiste)
+    def __init__(self, fileName):
+        self.file = open(fileName,"w")
 
-def createDB():
-    connexion = sqlite3.connect('example.db')
-    c = connexion.cursor()
-    c.execute('''CREATE TABLE artiste
-                 (id int, name text, url text, song int)''')
-    c.execute('''CREATE TABLE song
-                 (id int, name text, url text, accord text)''')
+    def insertSong(self, song, artiste):
+        Tsong=[artiste.name, song.title, song.url, song.accord]
+        self.file.writelines(str(Tsong).replace("'", "")[1:-1].replace(", ", ",")+"\n")
 
-    connexion.commit()
-    return connexion
-
-def insertSong(song, id, connexion):
-    connexion = sqlite3.connect('example.db')
-    c = connexion.cursor()
-    c.execute("INSERT INTO song VALUES ({0},'{1}','{2}','{3}')".format(id, song.title, song.link,"jklkj"))
-    connexion.commit()
-
-def insertArtiste(artiste, id, connexion):
-    connexion = sqlite3.connect('example.db')
-    c = connexion.cursor()
-    c.execute("INSERT INTO artiste VALUES ({0}, '{1}','{2}',{3})".format(id, artiste.name, artiste.link, artiste.nbSong))
-    connexion.commit()
-
+    def close(self):
+        self.file.close()
 
 
 class artiste:
 
     name = ""
-    link = ""
-    nbSong = 0
+    url = ""
     songs = []
 
-    def __init__(self, name, link, nbSong):
-        self.name = re.sub('[^A-Za-z0-9 '',]+', '', name)
-        self.link = link
-        self.nbSong = nbSong
+    def __init__(self, url):
+        self.name = url.split("/")[-1].replace(".php","").replace("-", " ")
+        self.url = url
+        self.songs = []
 
     def addSong(self, song):
         self.songs.append(song)
@@ -72,25 +53,20 @@ class artiste:
 class song:
 
     title = ""
-    link = ""
+    url = ""
     accord = []
 
-    def __init__(self, name, link, accord):
-        self.title = re.sub('[^A-Za-z0-9 '',]+', '', name)
-        self.link = link
+    def __init__(self, url, accord):
+        self.title = url.split("/")[-1].replace(".php","").replace("-", " ")#re.sub('[^A-Za-z0-9 '',]+', '', name).encode('ascii', 'ignore')
+        self.url = url
         self.accord = accord
-
-    def __str__(self):
-        return self.name + " : " + str(self.accord)
 
 
 
 
 def getArtistList(firstLettre):
     url = "http://www.boiteachansons.net/Partitions/index.php?artiste=" + str(firstLettre)
-    j = urlopen(url).read()
-
-    s = BeautifulSoup.BeautifulSoup(j,"lxml")
+    s=request(url)
 
     #recuperation de la liste des artistes
     artists = s.find_all('li', attrs={'class':'liElementLstPartitions'})
@@ -99,81 +75,68 @@ def getArtistList(firstLettre):
     artistList = []
     for a in artists:
         name = "".join(a.find_all('td')[-1].text.split("(")[:-1])[:-1]
-        link = a.find('a', attrs={'class':'aLiensChansons'})['href'].replace("..","http://www.boiteachansons.net")
-        nbSong = int(a.find_all('td')[-1].text.split("(")[-1].split("partition")[0])
+        url = a.find('a', attrs={'class':'aLiensChansons'})['href'].replace("..","http://www.boiteachansons.net")
 
-        artistList.append(artiste(name, link, nbSong))
+        artistList.append(artiste(url))
 
     return artistList
 
 
 def getSongsList(artiste):
     #recuperation de la liste des songs pour l artiste
-    url = artiste.link
-    j = urlopen(url).read()
-
-    s = BeautifulSoup.BeautifulSoup(j,"lxml")
+    url = artiste.url
+    s = request(url)
 
     songs = s.find_all('li', attrs={'class':'liElementLstPartitions'})
 
     for s in songs:
         name = s.find_all('span', attrs={'class':'sLstPart_TitreChanson'})[0].text
-        link = s.find('a', attrs={'class':'aLiensChansons'})['href'].replace("..","http://www.boiteachansons.net")
-        accord = getAccord(link)
-        artiste.addSong(song(name, link, accord))
+        url = s.find('a', attrs={'class':'aLiensChansons'})['href'].replace("..","http://www.boiteachansons.net")
+        accord = getAccord(url)
+        artiste.addSong(song(url, accord))
 
 
-def getAccord(link):
-    j = urlopen(link).read()
+def getAccord(url):
+    s = request(url)
 
-    s = BeautifulSoup.BeautifulSoup(j,"lxml")
-
-    accord = s.find_all('a', attrs={'class':'ac'})
+    accords = s.find_all('a', attrs={'class':'ac'})
 
     accordList = []
-    for a in accord:
-        accordList.append(a.text)
-
+    for accord in accords:
+        accordList.append(accord.text.encode('ascii', 'ignore'))
 
     return accordList
 
 
 
-#connexion = createDB()
-bdd=dbMongo()
-temp = ["9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-temp = ["9"]
-a=[]
-cnt = 0
-id=0
-for t in temp:
-    print t
-    b = getArtistList(t)
-    for i in range(len(b)):
+
+
+
+
+
+
+
+
+alphabet = ["9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+
+#pour tous les artistes (de A a Z + 9)
+for lettre in alphabet:
+    #Creation d'un fichier de sortie au format .txt avec pour nom la 1ere lettre des artistes
+    sortie = output(lettre + ".txt")
+
+    b = getArtistList(lettre)
+    print lettre +" - "+ str(len(b))
+
+    #pour tous les artistes commencant par la lettre "lettre"
+    for i in range(0, len(b)):
+        print"     "+str(i)+"-"+str(b[i].name)
         getSongsList(b[i])
-        #insertArtiste(b[i], id, connexion)
-        bdd.insertArtisteMongo(b[i],id)
+
+        #pour toutes les chansons de l'artiste
         for s in b[i].songs:
-            bdd.insertSongMongo(s,id)
-            #insertSong(s, id, connexion)
-        id+=1
-    a+=b
-#connexion.close()
-'''print "artiste :"+str(len(a))
-for aa in a:
-    cnt +=aa.nbSong
-print cnt'''
-'''a = getArtistList("9")
-getSongsList(a[1])
+            if(len(s.accord) != 0):
+                sortie.insertSong(s,b[i])
 
-for b in a[1].songs:
-    i=0
-    print b.name
-    for aa in b.accord:
-        i+=1
-        print aa+" "+str(i)
-    print b
 
-print s[0].link
-print s[0].name
-print s[0].accord'''
+    sortie.close()
