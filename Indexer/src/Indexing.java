12aefa4b8da678498e907d8fdd152c3066a606f7
ludo <ludovic.pfeiffer2@gmail.com>
2017-05-12 13:57:57 +0200

    Tokenization working really good

--- Indexer/src/Indexing.java
+++ Indexer/src/Indexing.java
@@ -1,32 +1,17 @@
-import org.apache.lucene.analysis.Analyzer;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.Tokenizer;
-import org.apache.lucene.analysis.shingle.ShingleFilter;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import utilities.NGramAnalyzer;
+import utilities.AccordAnalyzer;
 import utilities.ReadFiles;
-import org.apache.lucene.analysis.ngram.*;
+
 import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
 
 public class Indexing {
 
     public static void main(String[] args) {
         ReadFiles tFiles = new ReadFiles("../parsing/Lettre parsee", "readThread");
-            /*NGramTokenizer tokenizer = new NGramTokenizer(3, 3);
-            StringReader test = new StringReader(tFiles.getSongList().get(0).getAccords());
-            tokenizer.setReader(test);
-            tokenizer.reset();
-            CharTermAttribute termAtt = tokenizer.getAttribute(CharTermAttribute.class);
-            while (tokenizer.incrementToken()) {
-                String token = termAtt.toString();
-                System.out.println(token);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-        NGramAnalyzer tokenizer = new NGramAnalyzer(3, 3);
+        AccordAnalyzer tokenizer = new AccordAnalyzer(2, 8);
         TokenStream tokenStream;
         try {
             tokenStream = tokenizer
@@ -35,7 +20,9 @@ public class Indexing {
             while (tokenStream.incrementToken()) {
                 String text = tokenStream.getAttribute(CharTermAttribute.class)
                         .toString();
-                System.out.println(text);
+                int occurance = StringUtils.countMatches(text, " ");
+                if(occurance > 0)
+                    System.out.println(text);
             }
         } catch (IOException e) {
             e.printStackTrace();
--- Indexer/src/utilities/AccordAnalyzer.java
+++ Indexer/src/utilities/AccordAnalyzer.java
@@ -4,22 +4,21 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
-import org.apache.lucene.analysis.ngram.NGramTokenizer;
 import org.apache.lucene.analysis.shingle.ShingleFilter;
 
-public class NGramAnalyzer extends Analyzer {
+public class AccordAnalyzer extends Analyzer {
 
     private int min;
     private int max;
 
-    public NGramAnalyzer(int min, int max) {
+    public AccordAnalyzer(int min, int max) {
         this.min = min;
         this.max = max;
     }
     @Override
     protected TokenStreamComponents createComponents(String s) {
         Tokenizer standardTokenizer = new WhitespaceTokenizer();
-        TokenStream tok = new ShingleFilter(standardTokenizer, 3, 3);
+        TokenStream tok = new ShingleFilter(standardTokenizer, min, max);
         return new TokenStreamComponents(standardTokenizer, tok);
     }
 }
