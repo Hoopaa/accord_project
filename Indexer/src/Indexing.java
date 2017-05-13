29308bc8db5ee8db354700f2b190d58be5a8ee4d
Hoopaa <ludovic.pfeiffer2@gmail.com>
2017-05-13 14:52:07 +0200

    Retrieving working, need modifications for interface

--- Indexer/src/Indexing.java
+++ Indexer/src/Indexing.java
@@ -1,11 +1,10 @@
-import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import utilities.AccordAnalyzer;
@@ -19,44 +18,56 @@ import java.nio.file.Path;
 
 public class Indexing {
 
+
     public static void main(String[] args) {
+        boolean indexing = false;
         try {
-            ReadFiles tFiles = new ReadFiles("../parsing/Lettre parsee", "readThread");
-            AccordAnalyzer tokenizer = new AccordAnalyzer(2, 8);
-            IndexWriterConfig iwc = new IndexWriterConfig(tokenizer);
-            iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-            iwc.setUseCompoundFile(false);
             Path path = FileSystems.getDefault().getPath("index_standard");
             Directory dir = FSDirectory.open(path);
+            if(indexing) {
+                ReadFiles tFiles = new ReadFiles("../parsing/Lettre parsee", "readThread");
+                AccordAnalyzer tokenizer = new AccordAnalyzer(2, 8);
+                IndexWriterConfig iwc = new IndexWriterConfig(tokenizer);
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+                iwc.setUseCompoundFile(false);
 
-            IndexWriter indexWriter = new IndexWriter(dir, iwc);
+                IndexWriter indexWriter = new IndexWriter(dir, iwc);
 
-            TokenStream tokenStream;
-            for(Song s : tFiles.getSongList()) {
-                tokenStream = tokenizer
-                        .tokenStream("dummy", new StringReader(s.getAccords()));
-                tokenStream.reset();
-                Document d = new Document();
-                Field name = new StringField("name", s.getName(), Field.Store.YES);
-                Field author = new StringField("author", s.getArtistName(), Field.Store.YES);
-                Field url = new StringField("url", s.getUrl(), Field.Store.YES);
-                d.add(name);
-                d.add(author);
-                d.add(url);
-                while (tokenStream.incrementToken()) {
-                    String text = tokenStream.getAttribute(CharTermAttribute.class)
-                            .toString();
-                    Field accords = new StringField("accord", text, Field.Store.YES);
-                    d.add(accords);
+                TokenStream tokenStream;
+                for (Song s : tFiles.getSongList()) {
+                    tokenStream = tokenizer
+                            .tokenStream("dummy", new StringReader(s.getAccords()));
+                    tokenStream.reset();
+                    Document d = new Document();
+                    Field name = new StringField("name", s.getName(), Field.Store.YES);
+                    Field author = new StringField("author", s.getArtistName(), Field.Store.YES);
+                    Field url = new StringField("url", s.getUrl(), Field.Store.YES);
+                    d.add(name);
+                    d.add(author);
+                    d.add(url);
+                    while (tokenStream.incrementToken()) {
+                        String text = tokenStream.getAttribute(CharTermAttribute.class)
+                                .toString();
+                        if (text.length() > 0) {
+                            Field accords = new StringField("accord", text, Field.Store.YES);
+                            d.add(accords);
+                        }
+                    }
+                    tokenStream.reset();
+                    indexWriter.addDocument(d);
                 }
-                tokenStream.reset();
-                indexWriter.addDocument(d);
+                indexWriter.close();
             }
-            indexWriter.close();
             IndexReader reader = DirectoryReader.open(dir);
-            /*for(IndexableField i :reader.document(0).getFields("accord")) {
-                System.out.println(i.stringValue());
-            }*/
+            IndexSearcher is = new IndexSearcher(reader);
+            Query query = new TermQuery(new Term("accord", "G D C C"));
+            ScoreDoc[] hits = is.search(query, Integer.MAX_VALUE).scoreDocs;
+            System.out.println(hits.length);
+            for(ScoreDoc hit : hits) {
+                Document doc = is.doc(hit.doc);
+                System.out.println(doc.get("name") + " : " + doc.get("author") + " : " + hit.score);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
